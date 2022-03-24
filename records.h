7e55b70e5085a027598d5eb1308826ba49096605
DavidK1412 <davidcasallas013@gmail.com>
2022-03-24 12:35:48 -0500

    records.h

---
+++ records.h
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+/* Recibe: string1:"Recarga" string2:"3102391200"  string3:"3000"
+   Procedimiento: Abre el archivo trasnsactions.txt y escribe la transacción
+*/
+
+/* No recibe nada
+   Procedimiento: Busca las transacciones donde el número se ha visto involucrado e imprime
+   en pantalla la información de la transacción
+*/
